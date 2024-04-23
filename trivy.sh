5ac60ff9e76339614f14d4b7a35e5d926f06ce05
Maruti Nandan <61496370+mniet2009@users.noreply.github.com>
2024-04-23 10:43:48 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 # A Simple and Comprehensive Vulnerability Scanner for Containers and other Artifacts, Suitable for CI.
 
 sudo apt-get install wget apt-transport-https gnupg lsb-release
