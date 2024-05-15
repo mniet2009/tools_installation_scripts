34562e13c720cfadba0a9f3c95ce672df46fe3a5
Maruti Nandan <61496370+mniet2009@users.noreply.github.com>
2024-05-15 15:11:04 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,14 @@
+sudo apt update
+sudo apt install openjdk-17-jre
+java -version
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \   /usr/share/keyrings/jenkins-keyring.asc > /dev/null 
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \   https://pkg.jenkins.io/debian binary/ | sudo tee \   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
 #!/bin/bash
 
 sudo apt update -y
