42b2a2cacce37a9a13615d76be40be82a810fae4
Maruti Nandan <61496370+mniet2009@users.noreply.github.com>
2024-05-20 16:22:32 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,5 +1,5 @@
 # A Simple and Comprehensive Vulnerability Scanner for Containers and other Artifacts, Suitable for CI.
-
+#!/bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
